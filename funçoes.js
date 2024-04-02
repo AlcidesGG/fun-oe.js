c8e3165f3ca9e7654cc47487453bb62840f6de67
AlcidesGG <162999265+AlcidesGG@users.noreply.github.com>
2024-04-02 11:12:58 -0300

    Add files via upload

---
+++ funçoes.js
@@ -0,0 +1,33 @@
+function imprimirTexto(texto){
+    console.log(texto)
+}
+
+imprimirTexto("olá")
+
+function texto(text){
+    return text;
+}
+console.log(texto("parametro"))
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+
+function subtracao(numero1, numero2){
+    return numero1 - numero2
+}
+
+function multiplicacao(numero1, numero2){
+    return numero1 * numero2
+}
+
+function divisao(numero1, numero2){
+    return numero1 / numero2
+}
+
+console.log("a soma dos numeros e", soma(3,2))
+
+console.log("a subtracao dos numeros e", subtracao(3,2))
+
+console.log("a multiplicacao dos numeros e", multiplicacao(3,2))
+
+console.log("a divisao dos numeros e", divisao(3,2))
